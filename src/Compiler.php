bc59efa94b9cc6ba3fe6fbc0bd2e578860759fd0
Raffael Kessler <raffael.kessler@prowect.com>
2016-07-21 11:35:58 +0200

    DocBlocks hinzugefügt & Code formatiert

--- src/Compiler.php
+++ src/Compiler.php
@@ -5,13 +5,33 @@ namespace Drips\Markdown;
 use Drips\Utils\ICompiler;
 use Parsedown;
 
+/**
+ * Class Compiler
+ *
+ * Diese Klasse bietet eine einheitliche Schnittstelle (ICompiler) zu Übersetzen von Markdown- in HTML-Code.
+ * Als Compiler wird Parsedown verwendet. (siehe http://parsedown.org)
+ *
+ * @package Drips\Markdown
+ */
 class Compiler implements ICompiler
 {
+    /**
+     * Beinhaltet den eigentlichen Parsedown-Compiler (Objekt).
+     *
+     * @var Parsedown
+     */
     private static $compiler;
 
+    /**
+     * Der übergebene String (Markdown-Code) wird in HTML-Code übersetzt und anschließend als String zurückgeliefert.
+     *
+     * @param $string
+     *
+     * @return string
+     */
     public static function compile($string)
     {
-        if(static::$compiler == null){
+        if (static::$compiler == null) {
             static::$compiler = new Parsedown;
         }
 
--- tests/MarkdownCompilerTest.php
+++ tests/MarkdownCompilerTest.php
@@ -5,21 +5,27 @@ namespace tests;
 use Drips\Markdown\Compiler;
 use PHPUnit_Framework_TestCase;
 
-include __DIR__.'/../vendor/autoload.php';
-
 class MarkdownCompilerTest extends PHPUnit_Framework_TestCase
 {
     /**
-    * @dataProvider dateProvider
-    */
-   public function testMarkdownCompiler($markdown, $html)
-   {
-       $this->assertEquals(Compiler::compile($markdown), $html);
-   }
-   public function dateProvider()
-   {
-       return array(
-           array(file_get_contents(__DIR__."/test.md"), file_get_contents(__DIR__."/test.html"))
-       );
-   }
+     * Testfall zur Überprüfung ob Markdown-Code entsprechend in HTML-Code richtig übersetzt wird.
+     *
+     * @dataProvider dataProvider
+     */
+    public function testMarkdownCompiler($markdown, $html)
+    {
+        $this->assertEquals(Compiler::compile($markdown), $html);
+    }
+
+    /**
+     * Stellt Testdaten (Markdown- und HTML-Code) zur Verfügung
+     *
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return array(
+            array(file_get_contents(__DIR__ . "/test.md"), file_get_contents(__DIR__ . "/test.html"))
+        );
+    }
 }
